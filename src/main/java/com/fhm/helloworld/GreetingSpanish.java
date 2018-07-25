45ca9a97b885cf1542cf42c01aa61a1b1c394575
Francis Marti <Francis.X.Marti.-ND@disney.com>
2018-07-24 20:16:18 -0400

    Spanish support

---
+++ src/main/java/com/fhm/helloworld/GreetingSpanish.java
@@ -0,0 +1,17 @@
+/*
+
+ */
+package com.fhm.helloworld;
+
+/**
+ *
+ * @author Francis Marti <Francis.X.Marti.-ND@disney.com>
+ */
+public class GreetingSpanish implements Greeting{
+
+    @Override
+    public String message() {
+        return "Hola";
+    }   
+    
+}
